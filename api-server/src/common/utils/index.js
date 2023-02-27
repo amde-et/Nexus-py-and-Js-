4a50fda03135690df9c2355c913ae3e631bf0a60
Amdebirhan Abebe <amdecourse@gmail.com>
2023-02-27 08:03:31 -1200

    api-ser

---
+++ api-server/src/common/utils/index.js
@@ -0,0 +1,25 @@
+import { pick } from 'lodash';
+
+export {
+  getEncodedEmail,
+  decodeEmail,
+  getWaitMessage,
+  getWaitPeriod,
+  renderEmailChangeEmail,
+  renderSignUpEmail,
+  renderSignInEmail
+} from './auth';
+
+export const fixCompletedChallengeItem = obj =>
+  pick(obj, [
+    'id',
+    'completedDate',
+    'solution',
+    'githubLink',
+    'challengeType',
+    'files',
+    'isManuallyApproved'
+  ]);
+
+export const fixPartiallyCompletedChallengeItem = obj =>
+  pick(obj, ['id', 'completedDate']);
